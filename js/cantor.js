c98576e0a3577ac9487a6cf1823c00a67c5ac1cb
Felix Magnus <felix.p.magnus@gmail.com>
2017-04-09 14:18:17 +1100

    linting

--- js/cantor.js
+++ js/cantor.js
@@ -1,50 +1,50 @@
-//setup canvas
-let canvas = document.getElementById("fractalCanvas");
-let context = canvas.getContext('2d');
+// setup canvas
+const canvas = document.getElementById("fractalCanvas")
+const context = canvas.getContext('2d')
 
-//drawing parameters
-let startX = 50;
-let startY = 50;
-let initialLineLength = 750;
-let steppingDistanceY = 50;
-let drawType = "rect"; // can be "rect" or "line"
+// drawing parameters
+const startX = 50
+const startY = 50
+const initialLineLength = 750
+const steppingDistanceY = 50
+const drawType = "rect" // can be "rect" or "line"
 
-//start recursive cantor function
-cantor(startX, startY, initialLineLength);
+// start recursive cantor function
+cantor(startX, startY, initialLineLength)
 
-//draws a horizintal line
+// draws a horizintal line
 function drawHorizontalLine(x, y, length) {
-    context.beginPath();
-    context.moveTo(x, y);
-    context.lineTo(x + length, y);
-    context.stroke();
+    context.beginPath()
+    context.moveTo(x, y)
+    context.lineTo(x + length, y)
+    context.stroke()
 }
 
-//draws a rectangle
+// draws a rectangle
 function drawRect(x, y, width, height) {
-    context.fillRect(x, y, width, height);
+    context.fillRect(x, y, width, height)
 }
 
-//recursive cantor function
+// recursive cantor function
 function cantor(x, y, length) {
-    //exit condition: line length is smaller than 1 (not visible anyway)
+    // exit condition: line length is smaller than 1 (not visible anyway)
     if (length >= 1) {
-        //draw either rect or line at coordinates
+        // draw either rect or line at coordinates
         if (drawType === "rect")
             drawRect(x, y, length, Math.sqrt(length))
         else
-            drawHorizontalLine(x, y, length);
+            drawHorizontalLine(x, y, length)
 
-        //step to next layer
-        y += steppingDistanceY;
+        // step to next layer
+        y += steppingDistanceY
 
-        //padding between lines on one layer is min. 1
-        let padding = Math.max(1, length / 50);
+        // padding between lines on one layer is min. 1
+        const padding = Math.max(1, length / 50)
 
-        //"left" side --> draw line with half length - padding
-        cantor(x, y, length / 2 - padding);
+        // "left" side --> draw line with half length - padding
+        cantor(x, y, length / 2 - padding)
 
-        //"right" side --> draw line with half length - padding
-        cantor(x + length * 1 / 2 + padding, y, length / 2 - padding);
+        // "right" side --> draw line with half length - padding
+        cantor(x + length * 1 / 2 + padding, y, length / 2 - padding)
     }
 }
--- js/koch.js
+++ js/koch.js
@@ -1,87 +1,87 @@
 // setup canvas
-const canvas = document.getElementById("fractalCanvas");
-const context = canvas.getContext('2d');
+const canvas = document.getElementById("fractalCanvas")
+const context = canvas.getContext('2d')
 
 // drawing parameters
 const start = {
     x: 5,
     y: 250
-};
+}
 const end = {
     x: 845,
     y: 250
-};
+}
 
 // draws a line
 function drawLine(startPoint, endPoint) {
-    context.beginPath();
-    context.moveTo(startPoint.x, startPoint.y);
-    context.lineTo(endPoint.x, endPoint.y);
-    context.stroke();
+    context.beginPath()
+    context.moveTo(startPoint.x, startPoint.y)
+    context.lineTo(endPoint.x, endPoint.y)
+    context.stroke()
 }
 
 // erases a line
 function eraseLine(startPoint, endPoint) {
-    context.strokeStyle = 'rgba(255,255,255,1)';
-    context.lineWidth = 3;
-    drawLine(startPoint, endPoint);
-    context.strokeStyle = 'rgba(0,0,0,1)';
-    context.lineWidth = 1;
+    context.strokeStyle = 'rgba(255,255,255,1)'
+    context.lineWidth = 3
+    drawLine(startPoint, endPoint)
+    context.strokeStyle = 'rgba(0,0,0,1)'
+    context.lineWidth = 1
 }
 
 // recursive koch curve function
 function koch(startPoint, endPoint) {
     // calculate euclidean distance between both points
-    const diffX = startPoint.x - endPoint.x;
-    const diffY = startPoint.y - endPoint.y;
-    const distance = Math.sqrt(diffX ** 2 + diffY ** 2);
+    const diffX = startPoint.x - endPoint.x
+    const diffY = startPoint.y - endPoint.y
+    const distance = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2))
 
     // if distance is too small, exit recursion
     if (distance < 1) {
-        return;
+        return
     }
 
     // draw a line between both points
-    drawLine(startPoint, endPoint);
+    drawLine(startPoint, endPoint)
 
     // defined left split point on our line
     const left = {
         x: 2 / 3 * startPoint.x + 1 / 3 * endPoint.x,
         y: 2 / 3 * startPoint.y + 1 / 3 * endPoint.y
-    };
+    }
 
     // defined right split point on our line
     const right = {
         x: 1 / 3 * startPoint.x + 2 / 3 * endPoint.x,
         y: 1 / 3 * startPoint.y + 2 / 3 * endPoint.y
-    };
+    }
 
     // define middle point on our line
     const middle = {
         x: (left.x + right.x) / 2,
         y: (left.y + right.y) / 2
-    };
+    }
 
     // calculate perpendicular vector
-    const vectorXperpendicular = -(right.y - left.y);
-    const vectorYperpendicular = right.x - left.x;
+    const vectorXperpendicular = -(right.y - left.y)
+    const vectorYperpendicular = right.x - left.x
 
     // define top point of triangle via middle point + perpendicular vector
     const top = {
         x: middle.x + (Math.sqrt(3) / 2 * vectorXperpendicular),
         y: middle.y + (Math.sqrt(3) / 2 * vectorYperpendicular)
-    };
+    }
 
     // draw lines of triangle & erase bottom line
-    drawLine(left, top);
-    drawLine(right, top);
-    eraseLine(left, right);
+    drawLine(left, top)
+    drawLine(right, top)
+    eraseLine(left, right)
 
     // call koch function on all remaining parts of the line
-    koch(startPoint, left);
-    koch(left, top);
-    koch(top, right);
-    koch(right, endPoint);
+    koch(startPoint, left)
+    koch(left, top)
+    koch(top, right)
+    koch(right, endPoint)
 }
 
-koch(start, end);
+koch(start, end)
