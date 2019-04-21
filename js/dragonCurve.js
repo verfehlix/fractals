02255d21cb031e273c2f6b188e3b8ab967090eeb
Felix M <felix.p.magnus@gmail.com>
2019-04-21 17:19:27 +0200

    initial non working version

--- js/dragonCurve.js
+++ js/dragonCurve.js
@@ -19,13 +19,95 @@ function eraseLine(startPoint, endPoint) {
     context.lineWidth = 1
 }
 
+// calculates euclidean distance between two points
 function euclideanDistance(p1, p2) {
-    // calculate euclidean distance between both points
     const diffX = p1.x - p2.x
     const diffY = p1.y - p2.y
     return Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2))
 }
 
-drawLine({ x: 750, y: 250 }, { x: 100, y: 250 })
-drawLine({ x: 100, y: 250 }, { x: 400, y: 750 })
-drawLine({ x: 400, y: 750 }, { x: 750, y: 250 })
+function tri45getSideLengthFromBaseLine(baseLine) {
+    const baseLineLength = euclideanDistance(baseLine[0], baseLine[1])
+    return (baseLineLength * Math.sqrt(2)) / 2
+}
+
+function tri45getHeight(a, b, c) {
+    return (a * b) / c
+}
+
+function getNormalizedVectorFromLine(line) {
+    const x = line[0].x - line[1].x
+    const y = line[0].y - line[1].y
+    const vectorLength = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
+    return {
+        x: x / vectorLength,
+        y: y / vectorLength,
+    }
+}
+
+function getOrthogonalVectorClockWise(vector) {
+    return {
+        x: vector.y,
+        y: -vector.x,
+    }
+}
+
+function getOrthogonalVectorCounterClockWise(vector) {
+    return {
+        x: -vector.y,
+        y: vector.x,
+    }
+}
+
+// TODO: rotation immer abwechselnd!
+
+function calculateAngledChildLines(line, i) {
+    const childLineLength = tri45getSideLengthFromBaseLine(line)
+    const startPointLine1 = line[0]
+    const endPointLine2 = line[1]
+
+    const height = tri45getHeight(
+        childLineLength,
+        childLineLength,
+        euclideanDistance(line[0], line[1])
+    )
+
+    const middlePointBaseLine = {
+        x: (line[0].x + line[1].x) / 2,
+        y: (line[0].y + line[1].y) / 2,
+    }
+
+    const baseVector = getNormalizedVectorFromLine(line)
+    const orth1 = getOrthogonalVectorClockWise(baseVector)
+    const orth2 = getOrthogonalVectorCounterClockWise(baseVector)
+
+    const childLineCrossPoint = {
+        x: middlePointBaseLine.x + orth1.x * height,
+        y: middlePointBaseLine.y + orth1.y * height,
+    }
+
+    return [
+        [startPointLine1, childLineCrossPoint],
+        [childLineCrossPoint, endPointLine2],
+    ]
+}
+
+let i = 0
+const MAX_I = 2
+
+const initialLines = [[{ x: 200, y: 425 }, { x: 600, y: 425 }]]
+
+function dragonCurve(lineList) {
+    if (i === MAX_I) {
+        return
+    }
+
+    lineList.forEach(line => {
+        drawLine(line[0], line[1])
+        const childs = calculateAngledChildLines(line, i)
+        dragonCurve(childs)
+    })
+    i++
+}
+
+dragonCurve(initialLines)
